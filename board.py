17e730984b40fa6801688fbdc8d23cb8cefa9121
lucaskenji <ra1957066@gmail.com>
2021-02-17 19:00:18 -0300

    Updated get_row_number and get_col_number to deal with zero-indexed positions

--- board.py
+++ board.py
@@ -15,14 +15,13 @@ class Board:
     
     def get_row_number(self, position):
         # Receives position (e.g.: 1), returns the row this position is on the board.
-        return (position - 1) // 4
+        return position // 4
     
     def get_col_number(self, position):
         # There are four dark squares on each row where pieces can be placed.
-        # The remainder of (position / 4) is the number of a square in a certain row, 
-        # the only exception being 0 (which is actually the fourth square on each row.)
-        # Then we also take into account that odd rows on the board have a offset of 1 column.
-        remainder = (position % 4) if (position % 4) != 0 else 4
-        column_position = remainder + (remainder - 1)
+        # The remainder of (position / 4) can be used to determine which of the four squares has the position.
+        # We also take into account that odd rows on the board have a offset of 1 column.
+        remainder = position % 4
+        column_position = remainder * 2 # because the squares have a gap of one light square.
         is_row_odd = not (self.get_row_number(position) % 2 == 0)
-        return column_position if is_row_odd else column_position - 1
+        return column_position + 1 if is_row_odd else column_position
