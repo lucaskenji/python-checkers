37c636351b0ebcde375f82b0d2f3310ab4fd2d77
lucaskenji <ra1957066@gmail.com>
2021-02-17 19:43:48 -0300

    Added getters for piece class and tests for it

--- piece.py
+++ piece.py
@@ -2,3 +2,12 @@ class Piece:
     def __init__(self, name):
         # Example: <position><color><isKing?> 16WN
         self.name = name
+    
+    def getPosition(self):
+        return self.name[:-2]
+
+    def getColor(self):
+        return self.name[-2]
+
+    def isKing(self):
+        return True if self.name[-1] == 'Y' else False
--- tests/test_piece.py
+++ tests/test_piece.py
@@ -1,3 +1,9 @@
-#import sys
-#sys.path.append('..')
-#from piece import Piece
+import sys
+sys.path.append('..')
+from piece import Piece
+
+def test_piece_getters():
+    test_piece = Piece('16WN')
+    assert test_piece.getPosition() == '16'
+    assert test_piece.getColor() == 'W'
+    assert test_piece.isKing() == False
