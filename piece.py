3ad37c65b044baea3a03aabbb3cc1c27528ef558
lucaskenji <ra1957066@gmail.com>
2021-02-20 19:22:11 -0300

    Separated method to get adjacent(diagonal) squares, fixed some logical errors and commented get_moves

--- piece.py
+++ piece.py
@@ -12,9 +12,29 @@ class Piece:
     def is_king(self):
         return True if self.name[-1] == 'Y' else False
     
+    def get_adjacent_squares(self, board):
+        # Receives a Board object, returns at max four squares, all of which are potential moves
+        current_col = board.get_col_number(int(self.get_position()))
+        current_row = board.get_row_number(int(self.get_position()))
+        all_coords = []
+
+        if self.is_king():
+            all_coords = [(current_row - 1, current_col - 1), (current_row - 1, current_col + 1), (current_row + 1, current_col - 1), (current_row + 1, current_col + 1)]
+        else:
+            if board.get_color_up() == self.get_color():
+                all_coords = [(current_row - 1, current_col - 1), (current_row - 1, current_col + 1)]
+            else:
+                all_coords = [(current_row + 1, current_col - 1), (current_row + 1, current_col + 1)]
+        
+        # Because of the offset values above (+1 or -1), it's necessary to check if any invalid coordinates are on the list.
+        return list(filter(lambda coords: coords[0] != -1 and coords[0] != 8 and coords[1] != -1 and coords[1] != 8, all_coords))
+
     def get_moves(self, board):
+        # Receives a board, returns all possible moves. For more info check test specifications.
         def get_eat_position(piece, coords):
-            if (piece.get_color() == self.get_color()) or (coords[0] in (0, 7)) or (coords[1] in (0, 7)):
+            # Receives a piece that is obstructing this piece's way and its (row, column) coordinates.
+            # Returns the position to move in order to eat the piece, or None if it's impossible.
+            if (piece.get_color() == own_color) or (coords[0] in (0, 7)) or (coords[1] in (0, 7)):
                 return None
 
             if coords[1] > current_col:
@@ -30,17 +50,13 @@ class Piece:
         possible_moves = []
         own_color = self.get_color()
 
-        if self.is_king():
-            possible_coords = [(current_row - 1, current_col - 1), (current_row - 1, current_col + 1), (current_row + 1, current_col - 1), (current_row + 1, current_col + 1)]
-        else:
-            possible_coords = [(current_row - 1, current_col - 1), (current_row - 1, current_col + 1)] if board.get_color_up() == self.get_color() else [(current_row + 1, current_col - 1), (current_row + 1, current_col + 1)]
-
-        possible_coords = list(filter(lambda coords: coords[0] != -1 and coords[0] != 8 and coords[1] != -1 and coords[1] != 8, possible_coords))
+        possible_coords = self.get_adjacent_squares(board)
 
         close_squares = board.get_pieces_by_coords(*possible_coords)
         empty_squares = []
 
         for index, square in enumerate(close_squares):
+            # Empty squares are potential moves. Pieces are potential eating movements.
             if square is None:
                 empty_squares.append(index)
             else:
@@ -51,6 +67,7 @@ class Piece:
                 possible_moves.append(str(position_to_eat))
 
         if len(possible_moves) == 0:
+            # This is skipped if this piece can eat any other, because it is forced to eat it.
             for index in empty_squares:
                 new_position = (possible_coords[index][0] * 4) + (possible_coords[index][1] // 2)
                 possible_moves.append(str(new_position))
