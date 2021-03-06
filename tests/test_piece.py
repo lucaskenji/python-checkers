b06aa7b198f26d933449f7ff77f4e3c6953cebc7
lucaskenji <ra1957066@gmail.com>
2021-03-06 20:26:20 -0300

    Updated Piece test specifications

--- tests/test_piece.py
+++ tests/test_piece.py
@@ -14,21 +14,21 @@ def test_moves_free():
     test_piece = Piece('18WN')
     test_board = Board([test_piece], 'W')
     
-    assert test_piece.get_moves(test_board) == {'13', '14'}
+    assert test_piece.get_moves(test_board) == [{"position": '13', "eats_piece": False}, {"position": '14', "eats_piece": False}]
 
 def test_moves_bounds():
     # A piece cannot move outside of the board
     test_piece = Piece('8WN')
     test_board = Board([test_piece], 'W')
 
-    assert test_piece.get_moves(test_board) == {'4'}
+    assert test_piece.get_moves(test_board) == [{"position": '4', "eats_piece": False}]
 
 def test_moves_none():
     # A piece can't eat while getting outside of the board or eat with a piece on the way.
     test_piece = Piece('12WN')
     test_board = Board([test_piece, Piece('8BN'), Piece('9BN'), Piece('5BN')], 'W')
     
-    assert test_piece.get_moves(test_board) == set()
+    assert test_piece.get_moves(test_board) == []
 
 def test_moves_forced():
     # If a piece can eat another one, it can only do that.
@@ -36,7 +36,7 @@ def test_moves_forced():
     test_piece = Piece('12WN')
     test_board = Board([test_piece, Piece('9BN')], 'W')
 
-    assert test_piece.get_moves(test_board) == {'5'}
+    assert test_piece.get_moves(test_board) == [{"position": '5', "eats_piece": True}]
     pass
 
 def test_moves_eat_options():
@@ -44,11 +44,11 @@ def test_moves_eat_options():
     test_piece = Piece('13WN')
     test_board = Board([test_piece, Piece('9BN'), Piece('10BN')], 'W')
 
-    assert test_piece.get_moves(test_board) == {'4', '6'}
+    assert test_piece.get_moves(test_board) == [{"position": '4', "eats_piece": True}, {"position": '6', "eats_piece": True}]
 
 def test_moves_king():
     # A king can also move backwards.
     test_piece = Piece('4WY')
     test_board = Board([test_piece, Piece('8BN')], 'W')
 
-    assert test_piece.get_moves(test_board) == {'0', '1', '9'}
+    assert test_piece.get_moves(test_board) == [{"position": '0', "eats_piece": False}, {"position": '1', "eats_piece": False}, {"position": '9', "eats_piece": False}]
