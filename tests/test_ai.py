5c6ad757a49ddda769f08c5cdba9281d68797ca0
lucaskenji <ra1957066@gmail.com>
2021-03-29 18:29:00 -0300

    Changed tests for AI, implemented minimax and get_value, made small tweaks on Board

--- tests/test_ai.py
+++ tests/test_ai.py
@@ -7,17 +7,19 @@ from ai import AI
 def test_minimax_maximize():
     # minimax() returns the maximum value possible
     test_ai = AI('B')
-    test_board = Board([Piece('9WN'), Piece('13BN')], 'B')
+    test_board = Board([Piece('9WN'), Piece('17BN'), Piece('21BN')], 'B')
     assert test_ai.minimax(test_board, True, 1, 'B') == 1
+    assert test_ai.minimax(test_board, True, 2, 'B') == 1
 
 def test_minimax_minimize():
     # minimax() returns the minimum value possible
     test_ai = AI('B')
-    test_board = Board([Piece('9WN'), Piece('13BN')], 'B')
-    assert test_ai.minimax(test_board, False, 1, 'B') == 0
+    test_board = Board([Piece('9WN'), Piece('17BN'), Piece('21BN')], 'B')
+    assert test_ai.minimax(test_board, False, 1, 'B') == 1
+    assert test_ai.minimax(test_board, False, 2, 'B') == 0
 
 def test_get_move():
     # get_move() should return the move with the maximum value
     test_ai = AI('B')
-    test_board = Board([Piece('9WN'), Piece('13BN')], 'B')
-    assert test_ai.get_move(test_board) == {"position": "4", "eats_piece": True}
+    test_board = Board([Piece('9WN'), Piece('17BN'), Piece('21BN')], 'B')
+    assert test_ai.get_move(test_board) == {"position_from": "21", "position_to": "18", "eats_piece": True}
